d405f3164761e75aa98eb4781db7e21dffd13c49
alvivar <andresalvivar@gmail.com>
2015-06-23 01:31:13 -0600

    Rigidbody2D bullets, with auto follow target stuff, work-in-progress

---
+++ Core/Rigidbody2D/RigidBullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RigidBullet : MonoBehaviour
+{
+	public Transform target;
+	public float force;
+
+	private Rigidbody2D body;
+
+
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+		body.gravityScale = 0;
+		body.interpolation = RigidbodyInterpolation2D.Interpolate;
+	}
+
+
+	void Update()
+	{
+		if (force > 0)
+		{
+			Vector3 targetDir = (target.position - transform.position).normalized;
+			body.AddForce(targetDir * force);
+
+			this.tt("VelocityRefresh").ttAdd(() =>
+			{
+				body.velocity = Vector2.zero;
+			})
+			.ttAdd(2).ttWait();
+		}
+	}
+}

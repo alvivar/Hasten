7a0948a22a28a0d68d9f89535644ab04c5005428
alvivar <andresalvivar@gmail.com>
2015-07-14 19:59:30 -0600

    This static class is gonna hold all auxiliary mouse stuff

---
+++ Core/MouseUtil.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class MouseUtil : MonoBehaviour
+{
+	public static Vector3 GetMousePosition()
+	{
+		return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}

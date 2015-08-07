91bb8ad737de52134adee4d20617cdf1c377b08b
alvivar <andresalvivar@gmail.com>
2015-08-06 23:31:25 -0600

    Finally!

---
+++ Core/Motion/WalkJump.cs
@@ -0,0 +1,25 @@
+﻿
+// 2015/08/06 11:20:24 PM
+
+
+using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Motion))]
+public class WalkJump : MonoBehaviour
+{
+	private Motion motion;
+
+
+	void Start()
+	{
+		motion = GetComponent<Motion>();
+	}
+
+
+	void Update()
+	{
+
+	}
+}

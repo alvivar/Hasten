84af0f12f157ed24b94593c808013d76db7ff56b
alvivar <andresalvivar@gmail.com>
2015-08-07 11:42:57 -0600

    Walk jump v1, uncomplete.

--- Core/Motion/WalkJump.cs
+++ Core/Motion/WalkJump.cs
@@ -15,11 +15,66 @@ public class WalkJump : MonoBehaviour
 	void Start()
 	{
 		motion = GetComponent<Motion>();
+		motion.OnJump += OnJumpHandler;
 	}
 
 
 	void Update()
 	{
 
+	}
+
+
+	void OnJumpHandler()
+	{
+		// Ignore if grounded
+		if (motion.wallCollision.z != 0)
+			return;
+
+
+		if (motion.wallCollision.w != 0 || motion.wallCollision.y != 0)
+		{
+			motion.jumpCount = 0;
+		}
+
+
+		// Left wall
+		Vector2 wallJump = Vector2.zero;
+		if (motion.wallCollision.w != 0)
+		{
+			wallJump = (motion.jumpForce + new Vector2(motion.jumpForce.y * 0.60f, motion.jumpForce.y * 1.20f)) * 0.5f;
+
+			motion.Reset();
+			motion.jumpForce = wallJump;
+			motion.GetComponent<Rigidbody2D>().velocity = wallJump;
+			// motion.jumpForce = Vector2.ClampMagnitude(wallJump, motion.jumpForce.y);
+			// motion.jumpForce.y *= 1.30f;
+
+			InertiaCleanUp();
+		}
+
+
+		// Right wall
+		if (motion.wallCollision.y != 0)
+		{
+			wallJump = (motion.jumpForce + new Vector2(-motion.jumpForce.y * 0.60f, motion.jumpForce.y * 1.20f)) * 0.5f;
+
+			motion.Reset();
+			motion.jumpForce = wallJump;
+			motion.GetComponent<Rigidbody2D>().velocity = wallJump;
+			// motion.jumpForce = Vector2.ClampMagnitude(wallJump, motion.jumpForce.y);
+			// motion.jumpForce.y *= 1.30f;
+
+			InertiaCleanUp();
+		}
+	}
+
+
+	void InertiaCleanUp()
+	{
+		this.tt("InertiaCleanUp").ttReset().ttLoop(0.30f, (ttHandler t) =>
+		{
+
+		});
 	}
 }

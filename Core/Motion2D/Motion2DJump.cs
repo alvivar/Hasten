85ab5a1bb42c81ea73bf0a5138823b7109c297ce
alvivar <andresalvivar@gmail.com>
2015-08-17 15:36:01 -0600

    Jump is a public function now, some comments and force override

--- Core/Motion2D/Motion2DJump.cs
+++ Core/Motion2D/Motion2DJump.cs
@@ -10,8 +10,9 @@ using System.Collections;
 [RequireComponent(typeof(Motion2D))]
 public class Motion2DJump : MonoBehaviour
 {
-	public Vector2 force;
-	public float decay;
+	public Vector2 force; // 0 is neutral
+	public Vector2 forceOverride; // Overrides ^
+	public float decay; // How fast becames 0 after jump
 
 	private Motion2D motion;
 	private Rigidbody2D rbody;
@@ -28,24 +29,34 @@ public class Motion2DJump : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			Vector2 jumpForce = Vector2.zero;
-			jumpForce.x = force.x != 0 ? force.x : motion.force.x;
-			jumpForce.y = force.y != 0 ? force.y : motion.force.y;
-
-			motion.force = jumpForce;
-
-
-			// if (force.x != 0)
-			// 	rbody.velocity = new Vector2(rbody.velocity.x, 0);
-
-			// if (force.y != 0)
-			// 	rbody.velocity = new Vector2(0, rbody.velocity.y);
-
-
-			this.tt("JumpDecay").ttReset().ttLoop(decay, (ttHandler t) =>
-			{
-				motion.force = Vector2.Lerp(motion.force, Vector2.zero, t.deltaTime);
-			});
+			DoJump(force);
 		}
+	}
+
+
+	public void DoJump(Vector2 force)
+	{
+		// 0 is neutral as input
+		Vector2 jumpForce = Vector2.zero;
+		jumpForce.x = force.x != 0 ? force.x : motion.force.x;
+		jumpForce.y = force.y != 0 ? force.y : motion.force.y;
+
+
+		// Override
+		if (forceOverride != Vector2.zero)
+		{
+			jumpForce.x = forceOverride.x != 0 ? forceOverride.x : motion.force.x;
+			jumpForce.y = forceOverride.y != 0 ? forceOverride.y : motion.force.y;
+		}
+
+
+		// Jump representation
+		motion.force = jumpForce;
+
+
+		this.tt("JumpDecay").ttReset().ttLoop(decay, (ttHandler t) =>
+		{
+			motion.force = Vector2.Lerp(motion.force, Vector2.zero, t.deltaTime);
+		});
 	}
 }

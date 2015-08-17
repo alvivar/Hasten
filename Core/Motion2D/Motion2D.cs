defb918f41f916b7b9115dd75e0b09c9b2212537
alvivar <andresalvivar@gmail.com>
2015-08-16 19:09:10 -0600

    Now this component is able to calculate his walls "collision flags"

--- Core/Motion2D/Motion2D.cs
+++ Core/Motion2D/Motion2D.cs
@@ -16,15 +16,17 @@ public class Motion2D : MonoBehaviour
 	[Range(0, 10)] public float decay;
 	public float limit;
 
-
 	[Header("Constants")]
 	public Vector2 direction;
 	public float magnitude;
 
-
 	[Header("Forces")]
 	public Vector2 gravity;
 	public Vector2 force;
+
+	[Header("Walls")]
+	public LayerMask wallLayer;
+	public Vector4 wallsColliding; // Clock style; x = up, y = right, z = down, w = left
 
 
 	private Rigidbody2D rbody;
@@ -67,5 +69,33 @@ public class Motion2D : MonoBehaviour
 		// Velocity
 		rbody.velocity = Vector2.Lerp(rbody.velocity, movement, Time.deltaTime * attack);
 		rbody.velocity = Vector2.Lerp(rbody.velocity, Vector2.zero, Time.deltaTime * decay);
+	}
+
+
+	void CalculateWallCollision()
+	{
+		Vector3 pos = transform.position;
+
+		wallsColliding.w = Physics2D.Raycast(pos, Vector2.left, 1, wallLayer) ? 1 : 0;
+		wallsColliding.y = Physics2D.Raycast(pos, Vector2.right, 1,  wallLayer) ? 1 : 0;
+		wallsColliding.x = Physics2D.Raycast(pos, Vector2.up, 1,  wallLayer) ? 1 : 0;
+		wallsColliding.z = Physics2D.Raycast(pos, Vector2.down, 1, wallLayer) ? 1 : 0;
+
+		Debug.Log(wallsColliding);
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		CalculateWallCollision();
+	}
+
+
+	void OnCollisionExit2D(Collision2D other)
+	{
+		this.tt().ttAdd(0.10f, () =>
+		{
+			CalculateWallCollision();
+		});
 	}
 }

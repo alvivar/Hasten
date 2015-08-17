a97af3e9c8fe2d773efdf27d80d9b1d6a2a8d2f4
alvivar <andresalvivar@gmail.com>
2015-08-16 19:08:06 -0600

    2D Input -> 2D Axis + Clean up

---
+++ Core/Motion2D/Motion2DAxis.cs
@@ -0,0 +1,26 @@
+﻿
+// Andrés Villalobos ^ andresalvivar@gmail.com ^ twitter.com/matnesis
+// 2015/08/11 10:57:05 PM
+
+
+using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Motion2D))]
+public class Motion2DAxis : MonoBehaviour
+{
+	private Motion2D motion;
+
+
+	void Start()
+	{
+		motion = GetComponent<Motion2D>();
+	}
+
+
+	void Update()
+	{
+		motion.direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+}

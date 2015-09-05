df7439d103d7cef7f3b87ceea5306c02fc16bcd7
alvivar <andresalvivar@gmail.com>
2015-09-05 02:10:44 -0600

    I guess I can use this for static dark magic.

---
+++ Core/Utility/Utility.cs
@@ -0,0 +1,17 @@
+﻿
+// 2015/09/04 10:03:48 PM
+
+// Powerful stuff.
+
+
+using UnityEngine;
+using System.Collections;
+
+
+public class Utility
+{
+	public static void rayThunder2D()
+	{
+		// ?
+	}
+}

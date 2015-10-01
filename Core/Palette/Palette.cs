67db919143372df6ef7dc278d6e7d206770a31f2
alvivar <andresalvivar@gmail.com>
2015-09-30 22:09:17 -0600

    Super quick singleton component to define and access colors.

---
+++ Core/Palette/Palette.cs
@@ -0,0 +1,26 @@
+﻿
+// 2015/09/30 09:48:35 PM
+
+
+using UnityEngine;
+using System.Collections;
+
+
+public class Palette : MonoBehaviour
+{
+	private static Palette _instance;
+	public static Palette g
+	{
+		get
+		{
+			if (_instance == null)
+				_instance = FindObjectOfType<Palette>();
+
+			return _instance;
+		}
+	}
+
+
+	public Color grayLite;
+	public Color grayStrong;
+}

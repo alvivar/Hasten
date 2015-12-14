6a5e2290e841b0c7da705747ad96e86c6faa83da
alvivar <andresalvivar@gmail.com>
2015-12-14 14:15:36 -0600

    It's time to start saving this behaviours

---
+++ Core/Motion2D/React/ReactFollower.cs
@@ -0,0 +1,61 @@
+﻿
+// Locks the Motion direction to target.position.
+
+// @matnesis
+// 2015/12/13 04:25 PM
+
+
+using UnityEngine;
+using System.Collections;
+using matnesis.TeaTime;
+
+
+[RequireComponent(typeof(Motion2D))]
+[RequireComponent(typeof(Motion2DActions))]
+public class ReactFollower : ReactBase
+{
+	[Header("Info")]
+	public float distanceToTarget;
+
+	[Header("Config")]
+	public Transform target;
+	public float distanceToFollow;
+
+	private Motion2D motion;
+
+
+	void Start()
+	{
+		motion = GetComponent<Motion2D>();
+	}
+
+
+	public override bool Condition()
+	{
+		distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+		if (distanceToTarget > distanceToFollow)
+			return true;
+
+		return false;
+	}
+
+
+	public override IEnumerator Action()
+	{
+		motion.actions.target = target;
+		motion.actions.doFollowTarget.Play();
+
+		yield return null;
+	}
+
+
+	public override void Stop()
+	{
+		if (motion.actions.doFollowTarget.IsPlaying)
+		{
+			motion.actions.doFollowTarget.Stop();
+			motion.direction = Vector3.zero;
+		}
+	}
+}

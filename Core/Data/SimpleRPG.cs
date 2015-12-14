876081db706112ca6ff15907597f151d1c054eda
alvivar <andresalvivar@gmail.com>
2015-12-14 14:15:22 -0600

    Experimental simple reactive data for a quick rpg.

---
+++ Core/Data/SimpleRPG.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class SimpleRPG : MonoBehaviour
+{
+	public IntReactiveProp hp = new IntReactiveProp(0);
+	public IntReactiveProp stamina = new IntReactiveProp(0);
+	public IntReactiveProp xp = new IntReactiveProp(0);
+
+
+	void Update()
+	{
+		hp.SyncWithInspector();
+		stamina.SyncWithInspector();
+		xp.SyncWithInspector();
+	}
+}

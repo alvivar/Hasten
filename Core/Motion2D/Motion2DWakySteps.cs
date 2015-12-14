353878fc4400b19f2d0d9b97e5a369a9d8b1de41
alvivar <andresalvivar@gmail.com>
2015-12-14 00:10:17 -0600

    Better config

--- Core/Motion2D/Motion2DWakySteps.cs
+++ Core/Motion2D/Motion2DWakySteps.cs
@@ -1,5 +1,5 @@
 ﻿
-// Weird walk simulation with some rotations.
+// Weird walk by rotations.
 
 // @matnesis
 // 2015/12/12 02:37 PM
@@ -8,23 +8,33 @@
 using DG.Tweening;
 using UnityEngine;
 using matnesis.TeaTime;
-using DG.Tweening;
 
 
+[RequireComponent(typeof(Motion2D))]
 public class Motion2DWakySteps : MonoBehaviour
 {
-	public TeaTime wakyMovement = null;
+	[Header("Config")]
+	public bool enableWakySteps = true;
+	public float angle = 5;
+	public float duration = 0.20f;
+
+	private Motion2D motion;
+	private TeaTime wakyMovement;
 
 
 	void Start()
 	{
+		motion = GetComponent<Motion2D>();
+
+
+		// Waky
 		wakyMovement = this.tt().Stop().Add((ttHandler t) =>
 		{
-			t.WaitFor(transform.DORotate(new Vector3(0, 0, 5), 0.20f).WaitForCompletion());
+			t.WaitFor(transform.DORotate(new Vector3(0, 0, angle), duration).WaitForCompletion());
 		})
 		.Add((ttHandler t) =>
 		{
-			t.WaitFor(transform.DORotate(new Vector3(0, 0, -5), 0.20f).WaitForCompletion());
+			t.WaitFor(transform.DORotate(new Vector3(0, 0, -angle), duration).WaitForCompletion());
 		})
 		.Repeat();
 	}
@@ -32,7 +42,6 @@ public class Motion2DWakySteps : MonoBehaviour
 
 	void Update()
 	{
-		//
 		if (motion.direction.sqrMagnitude != 0)
 		{
 			wakyMovement.Play();

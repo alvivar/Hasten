1463a5225b824ee0ab2fee791e58c49e9511c56c
alvivar <andresalvivar@gmail.com>
2015-12-13 23:25:21 -0600

    Auto flip on X.

---
+++ Core/Motion2D/Motion2DPaperFlip.cs
@@ -0,0 +1,53 @@
+﻿
+// Automatic 2D flip effect.
+
+// @matnesis
+// 2015/12/13 11:05 PM
+
+
+using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+
+[RequireComponent(typeof(Motion2D))]
+public class Motion2DPaperFlip : MonoBehaviour
+{
+	public bool enableXFlip = false;
+	public float flipDuration = 0.20f;
+
+	private Motion2D motion;
+
+
+	void Start()
+	{
+		motion = GetComponent<Motion2D>();
+	}
+
+
+	void Update()
+	{
+		if (enableXFlip)
+		{
+			// Left
+			if (motion.direction.x > 0)
+			{
+				this.tt("xFlip").Loop(flipDuration, (ttHandler t) =>
+				{
+					transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1, 1, 1), t.deltaTime);
+				})
+				.Wait();
+			}
+
+			// Right
+			if (motion.direction.x < 0)
+			{
+				this.tt("-xFlip").Loop(flipDuration, (ttHandler t) =>
+				{
+					transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(-1, 1, 1), t.deltaTime);
+				})
+				.Wait();
+			}
+		}
+	}
+}

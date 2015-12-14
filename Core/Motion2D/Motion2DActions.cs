4eff3e2a34255f5415aab67cd5f35e13cefd57db
alvivar <andresalvivar@gmail.com>
2015-12-13 23:24:58 -0600

    Configurable Motion2D behaviours.

---
+++ Core/Motion2D/Motion2DActions.cs
@@ -0,0 +1,39 @@
+
+// Configurable Motion 2D behaviours.
+
+// @matnesis
+// 2015/12/13 06:11 PM
+
+
+using UnityEngine;
+using System.Collections;
+using matnesis.TeaTime;
+
+
+[RequireComponent(typeof(Motion2D))]
+public class Motion2DActions : MonoBehaviour
+{
+	[Header("Config")]
+	public Transform target;
+
+	// TeaTime
+	public TeaTime doFollowTarget;
+
+	private Motion2D motion;
+
+
+	void Start()
+	{
+		motion = GetComponent<Motion2D>();
+
+
+		doFollowTarget = this.tt().Add(0.10f, () =>
+		{
+			Vector3 dirToTarget = target.position - transform.position;
+			dirToTarget.z = 0;
+
+			motion.direction = dirToTarget.normalized;
+		})
+		.Repeat();
+	}
+}

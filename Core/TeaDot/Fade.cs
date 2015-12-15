69b8c7f285c9f304d50444aa53a78afc15c11391
alvivar <andresalvivar@gmail.com>
2015-12-14 18:07:34 -0600

    Fade effect a la JQuery :P (With the new Reactive props stuff)

---
+++ Core/TeaDot/Fade.cs
@@ -0,0 +1,40 @@
+﻿
+// Quick shoot fade animation.
+
+// @matnesis
+// 2015/12/14 05:02 PM
+
+
+using DG.Tweening;
+using UnityEngine;
+
+
+public class Fade : MonoBehaviour
+{
+	public BoolReactiveProp Show = new BoolReactiveProp(true);
+	public float fadeInDuration = 0.20f;
+	public float fadeOutDuration = 1;
+
+	private Renderer render;
+
+
+	void Start()
+	{
+		render = GetComponent<Renderer>();
+
+
+		Show.Suscribe(x =>
+		{
+			render.material.DOKill();
+
+			if (x) render.material.DOColor(Color.white, fadeInDuration);
+			else render.material.DOColor(Color.clear, fadeOutDuration);
+		});
+	}
+
+
+	void Update()
+	{
+		Show.SyncWithInspector();
+	}
+}

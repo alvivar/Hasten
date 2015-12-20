8119b22c08a3f8284d72e39bd45420f40e4bef51
alvivar <andresalvivar@gmail.com>
2015-12-19 22:27:51 -0600

    Calls are made using SyncWithInspector to make a validate the change and optimized for the Editor to avoid build problems

--- Core/Reactive/ReactivePropObserver.cs
+++ Core/Reactive/ReactivePropObserver.cs
@@ -10,8 +10,11 @@
 
 using System;
 using System.Reflection;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 
 // ^
@@ -27,6 +30,7 @@ class Reactive : Attribute { }
 // automatically, syncing ReactiveProps with the Inspector on classes with the
 // Attribute [Reactive].
 
+#if UNITY_EDITOR
 [InitializeOnLoad] // Autorun
 class ReactivePropObserver
 {
@@ -36,29 +40,68 @@ class ReactivePropObserver
 	}
 
 
+	static void SetPropertyToField(MonoBehaviour mono, FieldInfo field, string propertyName, string fieldValue)
+	{
+		// Current
+		System.Object currentObject = field.GetValue(mono);
+		Type currentType = currentObject.GetType();
+
+		// Get
+		PropertyInfo p = currentType.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+		FieldInfo f = currentType.GetField(fieldValue, BindingFlags.NonPublic | BindingFlags.Instance);
+
+		// Set
+		p.SetValue(currentObject, f.GetValue(currentObject), null);
+	}
+
+
 	static void Update()
 	{
 		// For all MonoBehaviours
-
 		MonoBehaviour[] allMonoBehaviours = GameObject.FindObjectsOfType<MonoBehaviour>();
 		foreach (MonoBehaviour mono in allMonoBehaviours)
 		{
-			// With the [Reactive] attribute
-
+			// On classes with the [Reactive] attribute
 			Type monoType = mono.GetType();
-
 			if (Attribute.GetCustomAttribute(monoType, typeof(Reactive)) != null)
 			{
-				// On Reactive fields
-
-				foreach (FieldInfo t in monoType.GetFields())
+				// On public fields
+				foreach (FieldInfo t in monoType.GetFields(BindingFlags.Public | BindingFlags.Instance))
 				{
-					// Inspector sync
+					// On Reactive Properties
 
-					if (t.FieldType.Equals(typeof(IntReactiveProp)))
+					// Bool
+					if (t.FieldType.Equals(typeof(BoolReactiveProp)))
+						(t.GetValue(mono) as BoolReactiveProp).SyncWithInspector();
+						// SetPropertyToField(mono, t, "Value", "boolValue");
+
+					// String
+					else if (t.FieldType.Equals(typeof(StringReactiveProp)))
+						(t.GetValue(mono) as StringReactiveProp).SyncWithInspector();
+						// SetPropertyToField(mono, t, "Value", "stringValue");
+
+					// Int
+					else if (t.FieldType.Equals(typeof(IntReactiveProp)))
 						(t.GetValue(mono) as IntReactiveProp).SyncWithInspector();
+						// SetPropertyToField(mono, t, "Value", "intValue");
+
+					// Float
+					else if (t.FieldType.Equals(typeof(FloatReactiveProp)))
+						(t.GetValue(mono) as FloatReactiveProp).SyncWithInspector();
+						// SetPropertyToField(mono, t, "Value", "floatValue");
+
+					// Vector3
+					else if (t.FieldType.Equals(typeof(Vector3ReactiveProp)))
+						(t.GetValue(mono) as Vector3ReactiveProp).SyncWithInspector();
+						// SetPropertyToField(mono, t, "Value", "vector3Value");
+
+					// Vector2
+					else if (t.FieldType.Equals(typeof(Vector2ReactiveProp)))
+						(t.GetValue(mono) as Vector2ReactiveProp).SyncWithInspector();
+						// SetPropertyToField(mono, t, "Value", "vector2Value");
 				}
 			}
 		}
 	}
 }
+#endif

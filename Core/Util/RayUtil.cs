e7130835ab05384541f004c8e39fe8aa1fffdf32
alvivar <andresalvivar@gmail.com>
2015-12-31 20:47:40 -0600

    Raycast stuff :D

---
+++ Core/Util/RayUtil.cs
@@ -0,0 +1,23 @@
+
+// Raycast util stuff.
+
+// @matnesis 2015/12/31 06:12 PM
+
+
+using UnityEngine;
+using System.Collections;
+
+
+public static class RayUtil
+{
+	public static Transform GetTransformAt(Vector3 origin, Vector3 direction, float length, LayerMask layer)
+	{
+		Transform t = null;
+
+		RaycastHit rayHit;
+		if (Physics.Raycast(origin, direction, out rayHit,  length, layer))
+			return rayHit.transform;
+
+		return null;
+	}
+}

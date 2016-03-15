26319580ee06869e0a80ffc67deafc13f988e60e
alvivar <andresalvivar@gmail.com>
2016-03-15 00:03:23 -0600

    Clean up

--- Core/Motion2D/Motion2D.cs
+++ Core/Motion2D/Motion2D.cs
@@ -1,7 +1,7 @@
 ﻿
 // Custom Rigidbody2D Motion a la CharacterController.
 
-// Andrés Villalobos ^ andresalvivar@gmail.com ^ twitter.com/matnesis
+// Andrés Villalobos ~ andresalvivar@gmail.com ~ twitter.com/matnesis
 // 2015/08/11 10:13 PM
 
 
@@ -13,149 +13,154 @@ using matnesis.TeaTime;
 [RequireComponent(typeof(CircleCollider2D))]
 public class Motion2D : MonoBehaviour
 {
-	// Axis shortcut
-	private Motion2DAxis _axis;
-	public Motion2DAxis axis
-	{
-		get { return _axis == null ? _axis = GetComponent<Motion2DAxis>() : _axis; }
-	}
+    // Axis shortcut
+    private Motion2DAxis _axis;
+    public Motion2DAxis axis
+    {
+        get { return _axis == null ? _axis = GetComponent<Motion2DAxis>() : _axis; }
+    }
 
 
-	// Jump shortcut
-	private Motion2DJump _jump;
-	public Motion2DJump jump
-	{
-		get { return _jump == null ? _jump = GetComponent<Motion2DJump>() : _jump; }
-	}
+    // Jump shortcut
+    private Motion2DJump _jump;
+    public Motion2DJump jump
+    {
+        get { return _jump == null ? _jump = GetComponent<Motion2DJump>() : _jump; }
+    }
 
 
-	// Wall jump shortcut
-	private Motion2DWallJump _wallJump;
-	public Motion2DWallJump wallJump
-	{
-		get { return _wallJump == null ? _wallJump = GetComponent<Motion2DWallJump>() : _wallJump; }
-	}
+    // Wall jump shortcut
+    private Motion2DWallJump _wallJump;
+    public Motion2DWallJump wallJump
+    {
+        get { return _wallJump == null ? _wallJump = GetComponent<Motion2DWallJump>() : _wallJump; }
+    }
 
 
-	// Actions shortcut
-	private Motion2DActions _actions;
-	public Motion2DActions actions
-	{
-		get { return _actions == null ? _actions = GetComponent<Motion2DActions>() : _actions; }
-	}
+    // Actions shortcut
+    private Motion2DActions _actions;
+    public Motion2DActions actions
+    {
+        get { return _actions == null ? _actions = GetComponent<Motion2DActions>() : _actions; }
+    }
 
 
-	// Connection to Rigidbody2D.velocity
-	public Vector2 velocity
-	{
-		get { return rbody.velocity; }
-		set { rbody.velocity = value; }
-	}
+    // Connection to Rigidbody2D.velocity
+    public Vector2 velocity
+    {
+        get { return rbody.velocity; }
+        set { rbody.velocity = value; }
+    }
 
 
-	[Header("Config")]
-	[Range(0, 10)] public float attack;
-	[Range(0, 10)] public float decay;
-	public float limit;
-	public float limitOverride;
+    [Header("Config")]
+    [Range(0, 10)]
+    public float attack;
+    [Range(0, 10)]
+    public float decay;
+    public float limit;
+    public float limitOverride;
 
-	[Header("Constants")]
-	public Vector2 direction;
-	public float magnitude;
+    [Header("Constants")]
+    public Vector2 direction;
+    public float magnitude;
 
-	[Header("Forces")]
-	public Vector2 gravity;
-	public Vector2 gravityOverride;
-	public Vector2 force;
-	public float forceLimit;
+    [Header("Forces")]
+    public Vector2 gravity;
+    public Vector2 gravityOverride;
+    public Vector2 force;
+    public float forceLimit;
 
-	[Header("Walls Detection")]
-	public float raycastLength = 0.25f;
-	public LayerMask wallLayer;
-	public Vector4 wallsColliding; // Clock style; x = up, y = right, z = down, w = left
+    [Header("Walls Detection")]
+    public float raycastLength = 0.25f;
+    public LayerMask wallLayer;
+    public Vector4 wallsColliding; // Clock style; x = up, y = right, z = down, w = left
 
-	private float colliderRadius;
+    private float colliderRadius;
 
-	private Rigidbody2D rbody;
-	private Collider2D collidr;
-	private Vector2 movement;
+    private Rigidbody2D rbody;
+    private Collider2D collidr;
+    private Vector2 movement;
 
-	// tt
-	private TeaTime wallCollisionDetection;
-
-
-	void Start()
-	{
-		// Required
-		rbody = GetComponent<Rigidbody2D>();
-		collidr = GetComponent<Collider2D>();
+    // tt
+    private TeaTime wallCollisionDetection;
 
 
-		// Rigidbody2D defaults
-		rbody.interpolation = RigidbodyInterpolation2D.Interpolate; // Smooth
-		rbody.gravityScale = 0; // We use our own gravity
+    void Start()
+    {
+        // Required
+        rbody = GetComponent<Rigidbody2D>();
+        collidr = GetComponent<Collider2D>();
 
 
-		// Collider radius
-		colliderRadius = Mathf.Max(collidr.bounds.extents.y, collidr.bounds.extents.x);
+        // Rigidbody2D defaults
+        rbody.interpolation = RigidbodyInterpolation2D.Interpolate; // Smooth
+        rbody.gravityScale = 0; // We use our own gravity
 
 
-		// > Wall collision calculation
-		wallCollisionDetection = this.tt().Add(0.10f, () =>
-		{
-			CalculateWallCollision();
-		})
-		.Repeat();
-	}
+        // Collider radius
+        colliderRadius = Mathf.Max(collidr.bounds.extents.y, collidr.bounds.extents.x);
 
 
-	void Update()
-	{
-		// Direction * magnitude
-		movement = transform.TransformDirection(direction.normalized);
-		movement *= magnitude;
+        // > Wall collision calculation
+        wallCollisionDetection = this.tt().Add(0.10f, () =>
+        {
+            wallsColliding = GetWallsColliding(transform.position, colliderRadius, raycastLength, wallLayer);
+        })
+        .Repeat();
+    }
 
 
-		// Gravity + Override
-		if (gravityOverride != Vector2.zero)
-			movement += gravityOverride;
-		else
-			movement += gravity;
+    void Update()
+    {
+        // Direction * magnitude
+        movement = transform.TransformDirection(direction.normalized);
+        movement *= magnitude;
 
 
-		// Limit + Override
-		if (limitOverride != 0)
-			movement = Vector2.ClampMagnitude(movement, limitOverride);
-		else
-			movement = Vector2.ClampMagnitude(movement, limit);
+        // Gravity + Override
+        if (gravityOverride != Vector2.zero)
+            movement += gravityOverride;
+        else
+            movement += gravity;
 
 
-		// Unlimited force
-		if (forceLimit != 0)
-			movement += Vector2.ClampMagnitude(force, forceLimit);
-		else
-			movement += force;
+        // Limit + Override
+        if (limitOverride != 0)
+            movement = Vector2.ClampMagnitude(movement, limitOverride);
+        else
+            movement = Vector2.ClampMagnitude(movement, limit);
 
 
-		// Velocity
-		rbody.velocity = Vector2.Lerp(rbody.velocity, movement, Time.deltaTime * attack);
-		rbody.velocity = Vector2.Lerp(rbody.velocity, Vector2.zero, Time.deltaTime * decay);
-	}
+        // Unlimited force
+        if (forceLimit != 0)
+            movement += Vector2.ClampMagnitude(force, forceLimit);
+        else
+            movement += force;
 
 
-	void CalculateWallCollision()
-	{
-		Vector3 pos = transform.position;
-
-		wallsColliding.w = Physics2D.Raycast(pos, Vector2.left, colliderRadius + raycastLength, wallLayer) ? 1 : 0;
-		wallsColliding.y = Physics2D.Raycast(pos, Vector2.right, colliderRadius + raycastLength,  wallLayer) ? 1 : 0;
-		wallsColliding.x = Physics2D.Raycast(pos, Vector2.up, colliderRadius + raycastLength,  wallLayer) ? 1 : 0;
-		wallsColliding.z = Physics2D.Raycast(pos, Vector2.down, colliderRadius + raycastLength, wallLayer) ? 1 : 0;
-	}
+        // Velocity
+        rbody.velocity = Vector2.Lerp(rbody.velocity, movement, Time.deltaTime * attack);
+        rbody.velocity = Vector2.Lerp(rbody.velocity, Vector2.zero, Time.deltaTime * decay);
+    }
 
 
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		CalculateWallCollision();
-	}
+    
+    Vector4 GetWallsColliding(Vector3 position, float radius, float rayLength, LayerMask layer)
+    {
+        Vector4 result = new Vector4(
+            Physics2D.Raycast(position, Vector2.up, radius + rayLength, layer) ? 1 : 0,
+            Physics2D.Raycast(position, Vector2.right, radius + rayLength, layer) ? 1 : 0,
+            Physics2D.Raycast(position, Vector2.down, radius + rayLength, layer) ? 1 : 0,
+            Physics2D.Raycast(position, Vector2.left, radius + rayLength, layer) ? 1 : 0
+        );
+
+        return result;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        wallsColliding = GetWallsColliding(transform.position, colliderRadius, raycastLength, wallLayer);
+    }
 }

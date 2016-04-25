2d89081a84acb9d36f03cbcfffa669f03cefa64e
alvivar <andresalvivar@gmail.com>
2016-04-24 18:08:45 -0600

    Maybe something like this could be useful

---
+++ Core/Camera2D/Camera2DOffsetCall.cs
@@ -0,0 +1,33 @@
+﻿
+// This component will call the camera 2D attention when triggered.
+// 2016/04/02 01:50 PM
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Camera2DOffsetCall : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter()
+    {
+
+    }
+
+
+    void OnTriggerExit()
+    {
+
+    }
+}

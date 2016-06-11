e457a2a59c3e2999a29b92d1795fc463e7250778
alvivar <andresalvivar@gmail.com>
2016-06-11 13:40:37 -0600

    Wrapper over PlayerPrefs that makes my life easier :)

---
+++ Core/Util/Prefs.cs
@@ -0,0 +1,67 @@
+﻿
+// Wrapper (kind of) of PlayerPrefs that works as I like.
+
+// @matnesis
+// 2016/06/03 09:45 PM
+
+
+using UnityEngine;
+
+public class Prefs : MonoBehaviour
+{
+    // @
+    // Boolean
+
+    public static bool Bool(string id)
+    {
+        int val = 0;
+
+        if (PlayerPrefs.HasKey(id)) val = PlayerPrefs.GetInt(id);
+        else PlayerPrefs.SetInt(id, val);
+
+        return val == 0 ? false : true;
+    }
+
+    public static void Bool(string id, bool data)
+    {
+        PlayerPrefs.SetInt(id, data ? 1 : 0);
+    }
+
+
+    // @
+    // Int
+
+    public static int Int(string id)
+    {
+        int val = 0;
+
+        if (PlayerPrefs.HasKey(id)) val = PlayerPrefs.GetInt(id);
+        else PlayerPrefs.SetInt(id, val);
+
+        return val;
+    }
+
+    public static void Int(string id, int data)
+    {
+        PlayerPrefs.SetInt(id, data);
+    }
+
+
+    // @
+    // Float
+
+    public static float Float(string id)
+    {
+        float val = 0;
+
+        if (PlayerPrefs.HasKey(id)) val = PlayerPrefs.GetFloat(id);
+        else PlayerPrefs.SetFloat(id, val);
+
+        return val;
+    }
+
+    public static void Float(string id, float data)
+    {
+        PlayerPrefs.SetFloat(id, data);
+    }
+}

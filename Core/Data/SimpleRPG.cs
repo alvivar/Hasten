83daf6e2d64c8062bc32429881aeecb97f3e7839
alvivar <andresalvivar@gmail.com>
2016-06-24 22:33:25 -0600

    Improved +1

--- Core/Data/SimpleRPG.cs
+++ Core/Data/SimpleRPG.cs
@@ -1,10 +1,21 @@
 ﻿
+// Reactive data for simple RPG behaviours.
+
+// @matnesis
+// 2016/06/20 07:32 PM
+
+
 using UnityEngine;
 
 [Reactive]
 public class SimpleRPG : MonoBehaviour
 {
+	[Header("Stats")]
 	public IntReactiveProp hp = new IntReactiveProp(3);
-	public IntReactiveProp stamina = new IntReactiveProp(3);
+	public IntReactiveProp stamina = new IntReactiveProp(5);
 	public IntReactiveProp coins = new IntReactiveProp(0);
+
+	[Header("Combat")]
+	public IntReactiveProp damage = new IntReactiveProp(1);
+	public IntReactiveProp knockback = new IntReactiveProp(1);
 }

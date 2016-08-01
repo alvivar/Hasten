1fc10fceca653dbaf1b244995dac1b57138f3f15
alvivar <andresalvivar@gmail.com>
2016-07-31 21:20:03 -0600

    Reactive properties can skip the first call (on suscription)

--- Core/Reactive/ReactiveProp.cs
+++ Core/Reactive/ReactiveProp.cs
@@ -1,8 +1,5 @@
 ﻿
-// @
 // Reactive Properties!
-
-// Soon.
 
 // @matnesis
 // 2015/12/12 02:40 PM
@@ -11,98 +8,96 @@
 using UnityEngine;
 using System;
 
-/// <summary>
-/// Generic Reactive Property.
-/// </summary>
 [Serializable]
 public class ReactiveProp<T>
 {
-	private Action<T> suscribers;
-	private T valueSent = default(T);
+    private Action<T> suscribers;
+    private T valueSent = default(T);
 
-	[SerializeField]
-	private T _value = default(T);
-	public T Value
-	{
-		get { return _value; }
+    [SerializeField]
+    private T _value = default(T);
+    public T Value
+    {
+        get { return _value; }
 
-		set
-		{
-			if (_value != null && _value.Equals(value))
-				return;
+        set
+        {
+            if (_value != null && _value.Equals(value))
+                return;
 
-			_value = value;
+            _value = value;
 
-			CallSuscribers();
-		}
-	}
-
-
-	public ReactiveProp(T initialValue)
-	{
-		Value = initialValue;
-	}
+            CallSuscribers();
+        }
+    }
 
 
-	public void Suscribe(Action<T> callback)
-	{
-		suscribers += callback;
-
-		if (valueSent != null) callback(_value);
-	}
+    public ReactiveProp(T initialValue)
+    {
+        Value = initialValue;
+    }
 
 
-	public void CallSuscribers()
-	{
-		if (valueSent != null && valueSent.Equals(_value))
-			return;
+    public void Suscribe(Action<T> callback, bool callOnSuscription = true)
+    {
+        suscribers += callback;
 
-		valueSent = _value;
-		if (suscribers != null) suscribers(_value);
-	}
+        if (callOnSuscription && valueSent != null)
+            callback(_value);
+    }
+
+
+    public void CallSuscribers()
+    {
+        if (valueSent != null && valueSent.Equals(_value))
+            return;
+
+        valueSent = _value;
+        if (suscribers != null) suscribers(_value);
+    }
 }
 
 
-// @
+// ^
 // Specialized properties that can be seeing on the Unity inspector.
 
 [Serializable]
 public class BoolReactiveProp : ReactiveProp<bool>
 {
-	public BoolReactiveProp(bool initialValue) : base(initialValue) { }
+    public BoolReactiveProp(bool initialValue) : base(initialValue) { }
 }
 
 
 [Serializable]
 public class StringReactiveProp : ReactiveProp<string>
 {
-	public StringReactiveProp(string initialValue) : base(initialValue) { }
+    public StringReactiveProp(string initialValue) : base(initialValue) { }
 }
 
 
 [Serializable]
 public class IntReactiveProp : ReactiveProp<int>
 {
-	public IntReactiveProp(int initialValue) : base(initialValue) { }
+    public IntReactiveProp(int initialValue) : base(initialValue) { }
 }
 
 
 [Serializable]
 public class FloatReactiveProp : ReactiveProp<float>
 {
-	public FloatReactiveProp(float initialValue) : base(initialValue) { }
+    public FloatReactiveProp(float initialValue) : base(initialValue) { }
 }
 
 
 [Serializable]
 public class Vector3ReactiveProp : ReactiveProp<Vector3>
 {
-	public Vector3ReactiveProp(Vector3 initialValue) : base(initialValue) { }
+    public Vector3ReactiveProp(Vector3 initialValue) : base(initialValue) { }
 }
 
 
 [Serializable]
 public class Vector2ReactiveProp : ReactiveProp<Vector2>
 {
-	public Vector2ReactiveProp(Vector2 initialValue) : base(initialValue) { }
+    public Vector2ReactiveProp(Vector2 initialValue) : base(initialValue) { }
 }

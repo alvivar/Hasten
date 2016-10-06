74537a5c5c775d5ac8d2d71f48d4e0659eed1ef4
alvivar <andresalvivar@gmail.com>
2016-10-05 21:24:28 -0600

    Unfinished but I really want this

---
+++ Core/Tools/CenterPivotAroundChildren.cs
@@ -0,0 +1,21 @@
+
+// #todo I kind of want to change the position of a parent, based on his
+// children.
+
+
+#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class CenterPivotAroundChildren : UnityEditor.Editor
+{
+    // [MenuItem("Tools/Center Pivot Around Children")]
+    static void Action()
+    {
+
+    }
+}
+
+#endif

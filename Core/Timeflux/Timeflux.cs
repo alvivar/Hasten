62e8a1e6c39e2253a9f608caa39f4e75725b77cd
alvivar <andresalvivar@gmail.com>
2016-10-05 21:23:42 -0600

    Timeflux is usable now, the idea is to handle timeScale through him

--- Core/Timeflux/Timeflux.cs
+++ Core/Timeflux/Timeflux.cs
@@ -1,20 +1,51 @@
 ﻿
 // Time manager.
 
-// Andrés Villalobos ^ andresalvivar@gmail.com ^ twitter.com/matnesis
-// 2015/10/19 04:54 PM
+// @matnesis
+// 2015/10/19 04:54 PM | 2016/09/24 07:18 PM
 
 
 using UnityEngine;
+using matnesis.TeaTime;
 
 public class Timeflux : MonoBehaviour
 {
-    public float scale = 1;
+    public FloatReactiveProp scale = new FloatReactiveProp(1);
 
 
-    void Update()
+    void Awake()
     {
-        Time.timeScale = scale;
-        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        scale.Suscribe(UpdateTimeScale);
+    }
+
+
+    void OnDestroy()
+    {
+        scale.Unsubscribe(UpdateTimeScale);
+    }
+
+
+    void UpdateTimeScale(float timeScale)
+    {
+        Time.timeScale = timeScale;
+        Time.fixedDeltaTime = 0.02f * timeScale; // Physics
+    }
+
+
+    public TeaTime FadeToScale(float timeScale, float duration)
+    {
+        float currentScale = Time.timeScale;
+        return this.tt("@fadeToScale").Reset().Loop(duration, (ttHandler t) =>
+        {
+            scale.Value = Mathf.Lerp(
+                currentScale, timeScale, Easef.Smoothstep(t.t)
+            );
+        });
+    }
+
+
+    public void Reset()
+    {
+        this.tt("@fadeToScale").Reset();
     }
 }

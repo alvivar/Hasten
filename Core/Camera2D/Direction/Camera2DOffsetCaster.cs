15e87084b682a5a91df5b49f0399b9918839d578
alvivar <andresalvivar@gmail.com>
2016-10-16 13:41:36 -0600

    A component to hold Camera2D offset information and another to sync the closest data on detection! #useful

---
+++ Core/Camera2D/Direction/Camera2DOffsetCaster.cs
@@ -0,0 +1,105 @@
+﻿
+// This component updates the Camera2D offset with the closest
+// Camera2DOffsetData.
+
+// @matnesis
+// 2016/10/09 06:14 PM
+
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using matnesis.TeaTime;
+
+public class Camera2DOffsetCaster : MonoBehaviour
+{
+    public float tolerance = 16; // Search every 32 units of distance
+    public LayerMask layerThatBlocks; // The layers that will block the activate
+
+    [Header("Info")]
+    public Vector3 lastPosition;
+    public List<Camera2DOffsetData> offsets;
+
+    private Camera2D cam;
+    private static MonoBehaviour mono;
+
+
+    void Start()
+    {
+        cam = Game.camera2D;
+        if (mono == null) mono = this;
+
+
+        // @
+        // This sequence updates the Camera2D offset with the closest
+        // Camera2DOffsetData.
+        {
+            Camera2DOffsetData latest = null;
+            lastPosition = Vector3.zero;
+
+            this.tt().Add(0.50f, (ttHandler t) =>
+            {
+                // Do they exists?
+                if (Camera2DOffsetData.all == null) return;
+
+                // Are we in a new position?
+                var snapPos = SnapVector(transform.position, tolerance);
+                if (snapPos != lastPosition)
+                {
+                    lastPosition = snapPos;
+
+                    // Sort them
+                    offsets = Camera2DOffsetData.all
+                        .OrderBy(x => (snapPos - x.transform.position).sqrMagnitude)
+                        .ToList();
+
+                    // Update if new
+                    if (offsets.Count > 0 && offsets[0] != latest)
+                    {
+                        // Ignore if there is something between us
+                        var tPos = transform.position;
+                        var oPos = offsets[0].transform.position;
+                        var dir = (oPos - tPos).normalized;
+                        var distance = Vector3.Distance(oPos, tPos);
+
+                        if (Physics2D.Raycast(transform.position, dir, distance, layerThatBlocks))
+                            return;
+
+
+                        // Interpolate
+                        latest = offsets[0];
+                        InterpolateCamera2DOffsetOverride(offsets[0].offset);
+
+                        Debug.Log("Camera2D :: New Override Offset = " + offsets[0].offset);
+                    };
+                }
+            })
+            .Repeat();
+        }
+    }
+
+
+    private void InterpolateCamera2DOffsetOverride(Vector3 newOffset)
+    {
+        // Interpolation
+        Vector3 current = cam.focusOffsetOverride;
+        mono.tt("@InterpolateCamera2DOffsetOverride").Reset().Loop(3f, (ttHandler t) =>
+        {
+            cam.focusOffsetOverride = Vector3.Lerp(
+                current,
+                newOffset,
+                Easef.EaseIn(t.t)
+            );
+        });
+    }
+
+
+    public static Vector3 SnapVector(Vector3 vector, float gridSize)
+    {
+        return new Vector3(
+            Mathf.Round(vector.x / gridSize) * gridSize,
+            Mathf.Round(vector.y / gridSize) * gridSize,
+            Mathf.Round(vector.z / gridSize) * gridSize
+        );
+    }
+}
---
+++ Core/Camera2D/Direction/Camera2DOffsetData.cs
@@ -0,0 +1,70 @@
+﻿
+// This component contains offset information for the camera and an static
+// reference to all like him.
+
+// @matnesis
+// 2016/10/09 06:14 PM
+
+
+using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Camera2DOffsetData : MonoBehaviour
+{
+    public Vector3 offset;
+    public static List<Camera2DOffsetData> all;
+    public Transform icon = null;
+
+
+    void Start()
+    {
+        if (all == null)
+            all = new List<Camera2DOffsetData>();
+
+        if (!all.Contains(this))
+            all.Add(this);
+
+
+        // We don't need the icon while playing
+        if (icon && Application.isPlaying)
+            GameObject.Destroy(icon.gameObject);
+    }
+
+
+    void OnDestroy()
+    {
+        if (all != null && all.Contains(this))
+            all.Remove(this);
+    }
+
+
+#if UNITY_EDITOR
+    void Update()
+    {
+        // @
+        // Get / create a cube to be used as icon.
+        if (!Application.isPlaying)
+        {
+            string name = "@icon";
+
+            // Find
+            if (icon == null)
+                icon = transform.FindChild(name);
+
+            // Or create
+            if (icon == null)
+            {
+                icon = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+                icon.name = name;
+                icon.parent = transform;
+            }
+
+            // Update
+            icon.localPosition = Vector3.zero;
+            icon.localScale = (Vector3.one + offset).normalized * 5;
+            icon.forward = offset.normalized;
+        }
+    }
+#endif
+}

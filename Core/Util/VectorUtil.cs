059bc2a4b8bf85800813fb2fdf4dbede48b13ebf
alvivar <andresalvivar@gmail.com>
2017-04-30 19:45:08 -0600

    Better arguments

--- Core/Util/VectorUtil.cs
+++ Core/Util/VectorUtil.cs
@@ -1,30 +1,28 @@
 
-// Useful vector stuff.
+// Useful Vector stuff.
 
-// @matnesis 2016/01/01 11:36 PM
+// @matnesis
+// 2016/01/01 11:36 PM
 
 
 using UnityEngine;
 
-
 public static class VectorUtil
 {
-	/// <summary>
-	/// Returns a Vector3 only with the strongest direction at 1.
-	/// </summary>
-	public static Vector3 RawDirection(Vector3 from, Vector3 to)
-	{
-		Vector3 direction = (to - from).normalized;
+    /// <summary>
+    /// Returns a Vector3 only with the strongest direction at 1.
+    /// </summary>
+    public static Vector3 RawDirection(Vector3 dir)
+    {
+        dir.x = Mathf.Abs(dir.x) > Mathf.Abs(dir.y) ? dir.x : 0;
+        dir.x = Mathf.Abs(dir.x) > Mathf.Abs(dir.z) ? dir.x : 0;
 
-		direction.x = Mathf.Abs(direction.x) > Mathf.Abs(direction.y) ? direction.x : 0;
-		direction.x = Mathf.Abs(direction.x) > Mathf.Abs(direction.z) ? direction.x : 0;
+        dir.y = Mathf.Abs(dir.y) > Mathf.Abs(dir.x) ? dir.y : 0;
+        dir.y = Mathf.Abs(dir.y) > Mathf.Abs(dir.z) ? dir.y : 0;
 
-		direction.y = Mathf.Abs(direction.y) > Mathf.Abs(direction.x) ? direction.y : 0;
-		direction.y = Mathf.Abs(direction.y) > Mathf.Abs(direction.z) ? direction.y : 0;
+        dir.z = Mathf.Abs(dir.z) > Mathf.Abs(dir.y) ? dir.z : 0;
+        dir.z = Mathf.Abs(dir.z) > Mathf.Abs(dir.x) ? dir.z : 0;
 
-		direction.z = Mathf.Abs(direction.z) > Mathf.Abs(direction.y) ? direction.z : 0;
-		direction.z = Mathf.Abs(direction.z) > Mathf.Abs(direction.x) ? direction.z : 0;
-
-		return (direction * 10).normalized;
-	}
+        return (dir * 10).normalized;
+    }
 }

a1bce16f9d0168c219b74aa45911edcd70dcd6f0
alvivar <andresalvivar@gmail.com>
2017-04-30 19:34:27 -0600

    Rotator!

---
+++ Core/Util/Physics2DRotator.cs
@@ -0,0 +1,26 @@
+﻿
+// Rotates an object to certain direction! xD
+
+// @matnesis
+// 2016/12/05 04:25 PM
+
+
+using UnityEngine;
+
+public class Physics2DRotator : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rbody;
+
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        rbody.MoveRotation(rbody.rotation + speed * Time.fixedDeltaTime);
+    }
+}

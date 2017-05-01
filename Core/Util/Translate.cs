54953155fa500323a088bb3c7580934625cda33d
alvivar <andresalvivar@gmail.com>
2017-04-30 19:41:59 -0600

    Simplest translation

---
+++ Core/Util/Translate.cs
@@ -0,0 +1,19 @@
+﻿
+// Translates an object to certain direction! xD
+
+// @matnesis
+// 2016/11/28 03:15 PM
+
+
+using UnityEngine;
+
+public class Translate : MonoBehaviour
+{
+    public Vector3 direction;
+
+
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime);
+    }
+}

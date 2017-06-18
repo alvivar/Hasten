0dfde8d094a0f9d7b87b0721b240ac46bbd12314
alvivar <andresalvivar@gmail.com>
2017-06-17 20:04:29 -0600

    Classic simple look at

---
+++ Core/Util/LookAt.cs
@@ -0,0 +1,24 @@
+﻿
+// Looks a target immediately.
+
+// @matnesis
+// 2017/04/13 07:12 PM
+
+
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 lookOffset;
+
+
+    void Update()
+    {
+        if (target)
+        {
+            transform.LookAt(target);
+            transform.eulerAngles += lookOffset;
+        }
+    }
+}

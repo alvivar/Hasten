20b07ef46adb9183a56ce0762cdfa7fabdcd651b
alvivar <andresalvivar@gmail.com>
2018-02-09 21:17:53 -0600

    The simplest bullet for 2D games

---
+++ Mechanics/Bullet/Bullet.cs
@@ -0,0 +1,45 @@
+﻿
+// A flexible bullet.
+
+// @matnesis
+// 2017/03/27 11:44 am
+
+
+using UnityEngine;
+
+[ReactiveInEditMode]
+public class Bullet : MonoBehaviour
+{
+    [Header("Config")]
+    public float speed = 1;
+    public Vector3 direction;
+
+    [Header("Required")]
+    public Rigidbody2D rbody;
+    public TrailRenderer trail;
+
+    [Header("Info")]
+    public Collision2D lastCollision;
+
+
+    public System.Action<Collision2D> OnCollision;
+
+
+    void Update()
+    {
+        var hasDirection = direction != Vector3.zero;
+
+        // Move towards
+        if (hasDirection) rbody.velocity = direction * speed;
+
+        // Motion trail
+        trail.enabled = hasDirection;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        lastCollision = other;
+        if (OnCollision != null) OnCollision(other);
+    }
+}
---
+++ Mechanics/Bullet/BulletShooter.cs
@@ -0,0 +1,68 @@
+﻿
+// @matnesis
+// 2017/03/27 12:11 pm
+
+
+using UnityEngine;
+using matnesis.TeaTime;
+using matnesis.Liteprint;
+
+public class BulletShooter : MonoBehaviour
+{
+    [Header("Required")]
+    public Transform bulletPrefab;
+
+    [Header("Config")]
+    public LayerMask collisionLayer;
+
+    [Header("Particles")]
+    public ParticleSystem ignitionParticles;
+    public ParticleSystem collisionParticles;
+
+
+    Transform GetBullet(Vector3 originPos)
+    {
+        return bulletPrefab.liteInstantiate(originPos, Quaternion.identity);
+    }
+
+
+    public void DirectShoot(Vector3 originPos, Vector3 direction, float speed)
+    {
+        var bullet = GetBullet(originPos).GetComponent<Bullet>();
+        bullet.direction = direction;
+        bullet.speed = speed;
+
+
+        // Collision handler
+        bullet.OnCollision = x =>
+        {
+            // Get out of here
+            bullet.transform.position = new Vector3(-999, -999, -999);
+            bullet.direction = Vector2.zero;
+            bullet.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            bullet.transform.liteRecycle();
+        };
+    }
+
+
+    public void FollowerShoot(Vector3 originPos, Transform target, float speed, float refreshRate)
+    {
+        var bullet = GetBullet(originPos).GetComponent<Bullet>();
+        bullet.speed = speed;
+
+
+        // Sequence that follows
+        Vector3 targetDir = Vector3.zero;
+        var follower = this.tt(bullet.GetInstanceID() + "").Loop(refreshRate, t =>
+        {
+            bullet.direction = (target.position - transform.position).normalized;
+        });
+
+
+        // Collision handler
+        bullet.OnCollision = x =>
+        {
+            follower.Reset();
+        };
+    }
+}

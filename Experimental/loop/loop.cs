7b5f1c89e26035d862f9541562f5ee712627c507
alvivar <andresalvivar@gmail.com>
2018-04-08 15:42:35 -0600

    Support to tween colors

--- Experimental/loop/loop.cs
+++ Experimental/loop/loop.cs
@@ -1,63 +1,52 @@
-﻿
-// Tween library API prototype using DOTween in the meantime.
+﻿// Tween library API prototype using DOTween in the meantime.
 
 // Andrés Villalobos | twitter.com/matnesis | andresalvivar@gmail.com
 // 2017/08/13 11:15 pm
 
-
 using System;
-using UnityEngine;
 using DG.Tweening;
 using DG.Tweening.Core;
+using UnityEngine;
 
-
-public class loop
-{
+public class loop {
     private Sequence dotween = DOTween.Sequence();
 
-
-    public static loop create()
-    {
+    public static loop create() {
         return new loop();
     }
 
-
-    public loop then(DOGetter<float> getter, DOSetter<float> setter, float endvalue, float time)
-    {
+    public loop then(DOGetter<float> getter, DOSetter<float> setter, float endvalue, float time) {
         dotween.Append(DOTween.To(getter, setter, endvalue, time));
 
         return this;
     }
 
+    public loop then(DOGetter<Color> getter, DOSetter<Color> setter, Color endvalue, float time) {
+        dotween.Append(DOTween.To(getter, setter, endvalue, time));
 
-    public loop then(Tweener tweener)
-    {
+        return this;
+    }
+
+    public loop then(Tweener tweener) {
         dotween.Append(tweener);
 
         return this;
     }
 
-
-    public loop then(TweenCallback callback)
-    {
+    public loop then(TweenCallback callback) {
         dotween.AppendCallback(callback);
 
         return this;
     }
 
-
-    public loop wait(float time)
-    {
+    public loop wait(float time) {
         dotween.AppendInterval(time);
 
         return this;
     }
 
-
-    public loop wait(Func<float> callback)
-    {
-        dotween.AppendCallback(() =>
-        {
+    public loop wait(Func<float> callback) {
+        dotween.AppendCallback(() => {
             loop.create()
                 .then(() => dotween.Pause())
                 .wait(callback())
@@ -67,15 +56,11 @@ public class loop
         return this;
     }
 
-
-    public YieldInstruction WaitForCompletion()
-    {
+    public YieldInstruction WaitForCompletion() {
         return dotween.WaitForCompletion();
     }
 
-
-    public loop repeat()
-    {
+    public loop repeat() {
         dotween.SetLoops(-1);
 
         return this;

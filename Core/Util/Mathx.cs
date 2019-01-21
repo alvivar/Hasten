3dd2fc6adb35c76cea357426b02695e1dc35493b
Andrés Villalobos <andres.villalobos@sweetrush.com>
2019-01-21 12:25:49 -0600

    Experimental static math stuff

--- Core/Util/Mathx.cs
+++ Core/Util/Mathx.cs
@@ -1,9 +1,7 @@
-
 // Useful Math formulas.
 
-// @matnesis
+// Andrés Villalobos * twitter.com/matnesis * andresalvivar@gmail.com
 // 2017/03/21 08:05 PM
-
 
 using UnityEngine;
 
@@ -13,17 +11,24 @@ public static class Mathx
     {
         while (angle < 0) angle += 360;
         while (angle > 360) angle -= 360;
-
         return angle;
     }
 
-
-    public static Vector3 SnapVector(Vector3 vector, float size)
+    public static Vector3 VectorSnap(Vector3 vector, float size)
     {
         return new Vector3(
             Mathf.Round(vector.x / size) * size,
             Mathf.Round(vector.y / size) * size,
             Mathf.Round(vector.z / size) * size
+        );
+    }
+
+    public static Vector3 VectorZ(Vector3 vector, float newZ = 0)
+    {
+        return new Vector3(
+            vector.x,
+            vector.y,
+            newZ
         );
     }
 }

257f22ff6549efde3d6f347d1e2c0576069d5a0b
Andrés Villalobos <andres.villalobos@sweetrush.com>
2019-01-21 12:27:37 -0600

    Experimental Tween stuff for the UES

---
+++ Experimental/Entween/TweenSystem.cs
@@ -0,0 +1,66 @@
+﻿// Andrés Villalobos * twitter.com/matnesis * andresalvivar@gmail.com
+// 2018/05/20 02:04 am
+
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+public class TweenSystem : ComponentSystem
+{
+	struct PosData
+	{
+		public ComponentArray<TweenPos> pos;
+		public readonly int Length;
+	}
+
+	[Inject] readonly PosData posData;
+
+	override protected void OnUpdate()
+	{
+		for (int i = 0; i < posData.Length; i++)
+		{
+			var pos = posData.pos[i];
+			if (!pos.done)
+			{
+				var tRate = 1 / pos.duration;
+				pos.t += tRate * Time.deltaTime;
+
+				if (pos.easeFunc != null)
+					pos.transform.localPosition = Vector3.Lerp(pos.from, pos.to, pos.easeFunc(pos.t));
+				else
+					pos.transform.localPosition = Vector3.Lerp(pos.from, pos.to, pos.t);
+
+				if (pos.t >= 1)
+					pos.done = true;
+			}
+
+		}
+	}
+}
+
+public static class EntweenExtension
+{
+	public static Transform tweenPos(this Transform t, Vector3 from, Vector3 to, float duration, System.Func<float, float> easeFunc = null)
+	{
+		var tw = t.gameObject.GetComponent<TweenPos>();
+		if (!tw) tw = t.gameObject.AddComponent<TweenPos>();
+		tw.Init();
+		tw.from = from;
+		tw.to = to;
+		tw.duration = duration;
+		tw.easeFunc = easeFunc;
+
+		var go = t.gameObject.GetComponent<GameObjectEntity>();
+		if (go)
+		{
+			go.enabled = false;
+			go.enabled = true;
+		}
+		else
+		{
+			go = t.gameObject.AddComponent<GameObjectEntity>();
+		}
+
+		return t;
+	}
+}
---
+++ Experimental/Entween/TweenTest.cs
@@ -0,0 +1,25 @@
+﻿// Andrés Villalobos * twitter.com/matnesis * andresalvivar@gmail.com
+// 2018/05/20 02:24 am
+
+using UnityEngine;
+
+public class TweenTest : MonoBehaviour
+{
+	void Start()
+	{
+		transform.tweenPos(
+			transform.position,
+			transform.position + new Vector3(-10, 0, 0), 2,
+			t => Easef.Smoothstep(t));
+
+		// transform
+		// 	.tweenPos(
+		// 		transform.position,
+		// 		transform.position + new Vector3(-10, 0, 0), 5,
+		// 		t => Easef.Smootherstep(t))
+		// 	.tweenPos(
+		// 		transform.position,
+		// 		transform.position + new Vector3(-10, 0, 0), 5,
+		// 		t => Easef.Smootherstep(t));
+	}
+}

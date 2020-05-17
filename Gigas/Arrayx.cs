bc9fedf6922da58ed39cd09de8bc5dfebf2896b2
Andrés Villalobos <andresalvivar@gmail.com>
2020-05-16 21:27:18 -0600

    Better documentation.

--- Gigas/Arrayx.cs
+++ Gigas/Arrayx.cs
@@ -1,12 +1,12 @@
 ﻿// Arrayx is the simples array list.
 
-// The idea is to use *Length* as index when *Elements* change, so we can resize
-// the array as much as we like, and also be able to iterate on *Length* for the
+// The idea is to use 'Length' as index when 'Elements' change, so we can resize
+// the array as much as we like, and also be able to iterate on 'Length' for the
 // real subset.
 
 public class Arrayx<T>
 {
+    public T[] Elements;
     public int Size;
     public int Length;
-    public T[] Elements;
 }

cf9bb8cd9c1ebae09183436d4c57e117ade72520
Andrés Villalobos <andresalvivar@gmail.com>
2020-05-27 20:33:20 -0600

    Static library to modify Arrayx

--- Gigas/Arrayx.cs
+++ Gigas/Arrayx.cs
@@ -4,9 +4,38 @@
 // the array as much as we like, and also be able to iterate on 'Length' for the
 // real subset.
 
+using System;
+using System.Collections.Generic;
+
 public class Arrayx<T>
 {
-    public T[] Elements;
-    public int Size;
-    public int Length;
+    public T[] Elements = new T[8];
+    public int Size = 8;
+    public int Length = 0;
+
+    public static Arrayx<T> Add(Arrayx<T> arrx, T component)
+    {
+        arrx.Elements[arrx.Length++] = component;
+
+        if (arrx.Length >= arrx.Size)
+        {
+            arrx.Size *= 2;
+            Array.Resize(ref arrx.Elements, arrx.Size);
+        }
+
+        return arrx;
+    }
+
+    public static Arrayx<T> RemoveAt(Arrayx<T> arrx, int indexToRemove)
+    {
+        // Overwrite
+
+        Array.Copy(
+            arrx.Elements, indexToRemove + 1,
+            arrx.Elements, indexToRemove,
+            arrx.Length - indexToRemove - 1);
+        arrx.Length--;
+
+        return arrx;
+    }
 }

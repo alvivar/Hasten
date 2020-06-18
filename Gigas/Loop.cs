ad7fc46baca4134bae9e08359b9ab77b4a926698
Andrés Villalobos <andresalvivar@gmail.com>
2020-06-18 11:34:46 -0600

    Experimental list of callbacks

---
+++ Gigas/Loop.cs
@@ -0,0 +1,55 @@
+﻿//  This is an unfinished experimental list of callbacks, to accomplish patterns
+//  like Tweens or IEnumerators but in Data Oriented patterns.
+
+using System.Collections.Generic;
+
+public class Loop
+{
+    Arrayx<Loopx> queue;
+
+    public void Get()
+    {
+
+    }
+
+    public void Add()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+}
+
+public class Loopx
+{
+    public float time;
+    public System.Action callback;
+    public Loopx next;
+
+    public Loopx(float time = 0, System.Action callback = null)
+    {
+        this.callback = callback;
+        this.time = time;
+    }
+}
+
+// A cache for the general structure, probably!
+
+// Dictionary<string, Arrayx<Loopx>> cache = new Dictionary<string, Arrayx<Loopx>>();
+
+// public Arrayx<Loopx> Get(string id)
+// {
+//     if (cache.ContainsKey(id))
+//         return cache[id];
+
+//     if (cache[id] == null)
+//         cache[id] = new Arrayx<Loopx>();
+
+//     var loop = new Loopx();
+//     cache[id].Add(loop);
+
+//     return cache[id];
+// }

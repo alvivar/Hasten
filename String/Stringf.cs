626f133ad3d65e4cdb0eee7ff25afa48a4fe1796
Andrés Villalobos <andresalvivar@gmail.com>
2020-09-05 08:27:13 -0600

    String utilities

---
+++ String/Stringf.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+public static class Stringf
+{
+    public static string MakeSafeForCode(string text)
+    {
+        text = Regex.Replace(text, "[^a-zA-Z0-9_]", "_", RegexOptions.None);
+        if (char.IsDigit(text[0])) text = "_" + text;
+
+        return text;
+    }
+
+    public static string CamelCase(string text)
+    {
+        return $"{char.ToLowerInvariant(text[0])}{text.Substring(1)}";
+    }
+}

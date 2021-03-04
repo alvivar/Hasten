2c7f1522a9a0674fc839ddd1234b1e15d60d3d1d
Andrés Villalobos <andresalvivar@gmail.com>
2021-03-03 23:29:03 -0600

    Play / Dev mode + Experimental Play shortcuts

---
+++ EditorTools/OnEditorChange.cs
@@ -0,0 +1,36 @@
+﻿// #if UNITY_EDITOR
+
+// using UnityEditor;
+// using UnityEditor.SceneManagement;
+// using UnityEngine;
+
+// @todo Cool, but need some work.
+
+// // Original script, found at
+// // https://stackoverflow.com/questions/35586103/unity3d-load-a-specific-scene-on-play-mode
+
+// [InitializeOnLoadAttribute]
+// public static class DefaultSceneLoader
+// {
+//     static DefaultSceneLoader()
+//     {
+//         EditorApplication.playModeStateChanged += LoadDefaultScene;
+//     }
+
+//     static void LoadDefaultScene(PlayModeStateChange state)
+//     {
+//         if (state == PlayModeStateChange.ExitingEditMode)
+//         {
+//             EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+//             Debug.Log($"PlayModeStateChange.ExitingEditMode at {Time.time}");
+//         }
+
+//         if (state == PlayModeStateChange.EnteredPlayMode)
+//         {
+//             EditorSceneManager.LoadScene(0);
+//             Debug.Log($"PlayModeStateChange.EnteredPlayMode at {Time.time}");
+//         }
+//     }
+// }
+
+// #endif
---
+++ EditorTools/PlayTools.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+
+public static class PlayTools
+{
+    [MenuItem("Game/Play")]
+    public static void Play()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
+
+        EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/01-Lobby.unity");
+        EditorApplication.isPlaying = true;
+    }
+
+    [MenuItem("Game/Dev")]
+    public static void Dev()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
+
+        EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/02-City.unity");
+    }
+}
+
+#endif

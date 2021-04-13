391057ba2847f8ebf39f1dd348ccf20ad5dcdfa1
Andrés Villalobos <andresalvivar@gmail.com>
2021-04-13 15:34:39 -0600

    Comment updated

--- Gigas/EntitySet.cs
+++ Gigas/EntitySet.cs
@@ -1,7 +1,8 @@
 // This file is auto-generated. Modifications won't be saved, be cool.
 
-// EntitySet is a static database of MonoBehaviour classes that are considered a
-// Entity, classes with '// !Gigas' somewhere in their file.
+// EntitySet is a static database of GameObjects (Entities) and MonoBehaviour
+// classes (Components). Check out 'Femto.cs' for more information about the
+// code generated.
 
 // Refresh with the menu item 'Tools/Gigas/Generate EntitySet.cs'
 

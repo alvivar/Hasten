8dbccd0836c88dd928a602dc01e01393f571746a
Andrés Villalobos <andresalvivar@gmail.com>
2021-04-13 15:33:57 -0600

    Comment out

--- EditorTools/PlayTools.cs
+++ EditorTools/PlayTools.cs
@@ -1,38 +1,38 @@
-﻿#if UNITY_EDITOR
+﻿// #if UNITY_EDITOR
 
-using UnityEditor;
+// using UnityEditor;
 
 // Experimental, unfinished, but useful menu item commands to Play and Dev
 // scenes.
 
-public static class PlayTools
-{
-    [MenuItem("Game/Play")]
-    public static void Play()
-    {
-        if (EditorApplication.isPlaying == true)
-        {
-            EditorApplication.isPlaying = false;
-            return;
-        }
+// public static class PlayTools
+// {
+//     [MenuItem("Game/Play")]
+//     public static void Play()
+//     {
+//         if (EditorApplication.isPlaying == true)
+//         {
+//             EditorApplication.isPlaying = false;
+//             return;
+//         }
 
-        EditorApplication.SaveCurrentSceneIfUserWantsTo();
-        EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/01-Lobby.unity");
-        EditorApplication.isPlaying = true;
-    }
+//         EditorApplication.SaveCurrentSceneIfUserWantsTo();
+//         EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/01-Lobby.unity");
+//         EditorApplication.isPlaying = true;
+//     }
 
-    [MenuItem("Game/Dev")]
-    public static void Dev()
-    {
-        if (EditorApplication.isPlaying == true)
-        {
-            EditorApplication.isPlaying = false;
-            return;
-        }
+//     [MenuItem("Game/Dev")]
+//     public static void Dev()
+//     {
+//         if (EditorApplication.isPlaying == true)
+//         {
+//             EditorApplication.isPlaying = false;
+//             return;
+//         }
 
-        EditorApplication.SaveCurrentSceneIfUserWantsTo();
-        EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/02-City.unity");
-    }
-}
+//         EditorApplication.SaveCurrentSceneIfUserWantsTo();
+//         EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/02-City.unity");
+//     }
+// }
 
-#endif
+// #endif

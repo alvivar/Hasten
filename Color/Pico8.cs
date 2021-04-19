671d31479cbf9a824b62ab80aff5803bc18f442e
Andrés Villalobos <andresalvivar@gmail.com>
2021-04-19 14:58:46 -0600

    Pico 8 palette, yeah

---
+++ Color/Pico8.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class Pico8
+{
+    public static Color Black = new Color32(0x00, 0x00, 0x00, 0xFF);
+    public static Color DarkBlue = new Color32(0x1D, 0x2B, 0x53, 0xFF); // 1D2B53FF
+    public static Color DarkPurple = new Color32(0x7E, 0x25, 0x53, 0xFF);
+    public static Color DarkGreen = new Color32(0x00, 0x87, 0x51, 0xFF);
+    public static Color Brown = new Color32(0xAB, 0x52, 0x36, 0xFF);
+    public static Color DarkGrey = new Color32(0x5F, 0x57, 0x4F, 0xFF); // 5F574FFF
+    public static Color LightGrey = new Color32(0xC2, 0xC3, 0xC7, 0xFF);
+    public static Color White = new Color32(0xFF, 0xF1, 0xE8, 0xFF); // FFF1E8FF
+    public static Color Red = new Color32(0xFF, 0x00, 0x4D, 0xFF);
+    public static Color Orange = new Color32(0xFF, 0xA3, 0x00, 0xFF);
+    public static Color Yellow = new Color32(0xFF, 0xEC, 0x27, 0xFF); // FFEC27FF
+    public static Color Green = new Color32(0x00, 0xE4, 0x36, 0xFF);
+    public static Color Blue = new Color32(0x29, 0xAD, 0xFF, 0xFF); // 29ADFFFF
+    public static Color Lavender = new Color32(0x83, 0x76, 0x9C, 0xFF);
+    public static Color Pink = new Color32(0xFF, 0x77, 0xA8, 0xFF);
+    public static Color LightPeach = new Color32(0xFF, 0xCC, 0xAA, 0xFF);
+}

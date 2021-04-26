36bd542b08a7c54c399159d2ba8324d353c55ede
Andrés Villalobos <andresalvivar@gmail.com>
2021-04-26 15:12:00 -0600

    String colors as comments

--- Color/Pico8.cs
+++ Color/Pico8.cs
@@ -2,20 +2,20 @@
 
 public static class Pico8
 {
-    public static Color Black = new Color32(0x00, 0x00, 0x00, 0xFF);
+    public static Color Black = new Color32(0x00, 0x00, 0x00, 0xFF); // 000000FF
     public static Color DarkBlue = new Color32(0x1D, 0x2B, 0x53, 0xFF); // 1D2B53FF
-    public static Color DarkPurple = new Color32(0x7E, 0x25, 0x53, 0xFF);
-    public static Color DarkGreen = new Color32(0x00, 0x87, 0x51, 0xFF);
-    public static Color Brown = new Color32(0xAB, 0x52, 0x36, 0xFF);
+    public static Color DarkPurple = new Color32(0x7E, 0x25, 0x53, 0xFF); // 7E2553FF
+    public static Color DarkGreen = new Color32(0x00, 0x87, 0x51, 0xFF); // 008751FF
+    public static Color Brown = new Color32(0xAB, 0x52, 0x36, 0xFF); // AB5236FF
     public static Color DarkGrey = new Color32(0x5F, 0x57, 0x4F, 0xFF); // 5F574FFF
-    public static Color LightGrey = new Color32(0xC2, 0xC3, 0xC7, 0xFF);
+    public static Color LightGrey = new Color32(0xC2, 0xC3, 0xC7, 0xFF); // C2C3C7FF
     public static Color White = new Color32(0xFF, 0xF1, 0xE8, 0xFF); // FFF1E8FF
-    public static Color Red = new Color32(0xFF, 0x00, 0x4D, 0xFF);
-    public static Color Orange = new Color32(0xFF, 0xA3, 0x00, 0xFF);
+    public static Color Red = new Color32(0xFF, 0x00, 0x4D, 0xFF); // FF004DFF
+    public static Color Orange = new Color32(0xFF, 0xA3, 0x00, 0xFF); // FFA300FF
     public static Color Yellow = new Color32(0xFF, 0xEC, 0x27, 0xFF); // FFEC27FF
-    public static Color Green = new Color32(0x00, 0xE4, 0x36, 0xFF);
+    public static Color Green = new Color32(0x00, 0xE4, 0x36, 0xFF); // 00E436FF
     public static Color Blue = new Color32(0x29, 0xAD, 0xFF, 0xFF); // 29ADFFFF
-    public static Color Lavender = new Color32(0x83, 0x76, 0x9C, 0xFF);
-    public static Color Pink = new Color32(0xFF, 0x77, 0xA8, 0xFF);
-    public static Color LightPeach = new Color32(0xFF, 0xCC, 0xAA, 0xFF);
+    public static Color Lavender = new Color32(0x83, 0x76, 0x9C, 0xFF); // 83769CFF
+    public static Color Pink = new Color32(0xFF, 0x77, 0xA8, 0xFF); // FF77A8FF
+    public static Color LightPeach = new Color32(0xFF, 0xCC, 0xAA, 0xFF); // FFCCAAFF
 }

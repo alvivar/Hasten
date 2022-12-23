759b4805766edb7f6dc7c1019e093d3072d3b516
Andrés Villalobos <andresalvivar@gmail.com>
2022-12-23 00:37:14 -0600

    Updated from deprecated.

--- EditorTools/PlayTools.cs
+++ EditorTools/PlayTools.cs
@@ -1,38 +1,36 @@
-﻿// #if UNITY_EDITOR
+﻿#if UNITY_EDITOR
 
-// using UnityEditor;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 
-// Experimental, unfinished, but useful menu item commands to Play and Dev
-// scenes.
+public static class PlayTools
+{
+    [MenuItem("Game/Play")]
+    public static void Play()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
 
-// public static class PlayTools
-// {
-//     [MenuItem("Game/Play")]
-//     public static void Play()
-//     {
-//         if (EditorApplication.isPlaying == true)
-//         {
-//             EditorApplication.isPlaying = false;
-//             return;
-//         }
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Game/Scenes/Multiplayer/01-Lobby.unity");
+        EditorApplication.isPlaying = true;
+    }
 
-//         EditorApplication.SaveCurrentSceneIfUserWantsTo();
-//         EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/01-Lobby.unity");
-//         EditorApplication.isPlaying = true;
-//     }
+    [MenuItem("Game/Dev")]
+    public static void Dev()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
 
-//     [MenuItem("Game/Dev")]
-//     public static void Dev()
-//     {
-//         if (EditorApplication.isPlaying == true)
-//         {
-//             EditorApplication.isPlaying = false;
-//             return;
-//         }
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Game/Scenes/Multiplayer/02-City.unity");
+    }
+}
 
-//         EditorApplication.SaveCurrentSceneIfUserWantsTo();
-//         EditorApplication.OpenScene("Assets/Game/Scenes/Multiplayer/02-City.unity");
-//     }
-// }
-
-// #endif
+#endif

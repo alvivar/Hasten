1efa126af9b1c69461dfcb9787632615e5de56cc
Andrés Villalobos <andresalvivar@gmail.com>
2023-07-16 16:04:36 -0600

    Commented because BestHTTP2 is a requirement.

--- Bite/Best HTTP/BITEBestWS.cs
+++ Bite/Best HTTP/BITEBestWS.cs
@@ -1,146 +1,146 @@
-using BestHTTP.WebSocket;
-using BITE;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
+// using BestHTTP.WebSocket;
+// using BITE;
+// using System;
+// using System.Collections.Generic;
+// using System.Text;
+// using UnityEngine;
 
-public class BITEBestWS : MonoBehaviour
-{
-    public Action<Frame> OnFrameReceived;
+// public class BITEBestWS : MonoBehaviour
+// {
+//     public Action<Frame> OnFrameReceived;
 
-    public int QueueLength { get { return frameQueue.Bytes.Length; } }
+//     public int QueueLength { get { return frameQueue.Bytes.Length; } }
 
-    [Header("Config")]
-    public string ip = "167.99.58.31"; // 127.0.0.1
-    public int port = 1991; // 1983
+//     [Header("Config")]
+//     public string ip = "167.99.58.31"; // 127.0.0.1
+//     public int port = 1991; // 1983
 
-    [Header("Info")]
-    public bool connected = false;
-    public int id = -1;
-    public int messageCount = 0;
+//     [Header("Info")]
+//     public bool connected = false;
+//     public int id = -1;
+//     public int messageCount = 0;
 
-    private Dictionary<int, Action<Frame>> callbacks = new Dictionary<int, Action<Frame>>();
-    private Frames framesReceived = new Frames();
-    private Frame frameQueue = new Frame();
-    private WebSocket webSocket;
+//     private Dictionary<int, Action<Frame>> callbacks = new Dictionary<int, Action<Frame>>();
+//     private Frames framesReceived = new Frames();
+//     private Frame frameQueue = new Frame();
+//     private WebSocket webSocket;
 
-    public void Start()
-    {
-        var address = $"ws://{ip}:{port}/ws";
-        webSocket = new WebSocket(new Uri(address));
+//     public void Start()
+//     {
+//         var address = $"ws://{ip}:{port}/ws";
+//         webSocket = new WebSocket(new Uri(address));
 
-        webSocket.OnOpen += OnOpen;
-        webSocket.OnMessage += OnMessageReceived;
-        webSocket.OnBinary += OnBinaryReceived;
-        webSocket.OnClosed += OnClosed;
-        webSocket.OnError += OnError;
+//         webSocket.OnOpen += OnOpen;
+//         webSocket.OnMessage += OnMessageReceived;
+//         webSocket.OnBinary += OnBinaryReceived;
+//         webSocket.OnClosed += OnClosed;
+//         webSocket.OnError += OnError;
 
-        webSocket.Open();
-    }
+//         webSocket.Open();
+//     }
 
-    void OnDestroy()
-    {
-        if (webSocket != null)
-        {
-            webSocket.Close();
-            webSocket = null;
-        }
-    }
+//     void OnDestroy()
+//     {
+//         if (webSocket != null)
+//         {
+//             webSocket.Close();
+//             webSocket = null;
+//         }
+//     }
 
-    void OnOpen(WebSocket ws)
-    {
-        Debug.Log("BITE WS Opened!");
-    }
+//     void OnOpen(WebSocket ws)
+//     {
+//         Debug.Log("BITE WS Opened!");
+//     }
 
-    void OnMessageReceived(WebSocket ws, string message)
-    {
-        Debug.Log($"BITE WS Message Received ({message.Length}): {message}");
-    }
+//     void OnMessageReceived(WebSocket ws, string message)
+//     {
+//         Debug.Log($"BITE WS Message Received ({message.Length}): {message}");
+//     }
 
-    void OnBinaryReceived(WebSocket ws, byte[] message)
-    {
-        framesReceived.Feed(message);
-        while (framesReceived.ProcessingCompleteFrames()) ;
-        while (framesReceived.HasCompleteFrame)
-        {
-            connected = true;
+//     void OnBinaryReceived(WebSocket ws, byte[] message)
+//     {
+//         framesReceived.Feed(message);
+//         while (framesReceived.ProcessingCompleteFrames()) ;
+//         while (framesReceived.HasCompleteFrame)
+//         {
+//             connected = true;
 
-            var frame = framesReceived.Dequeue();
+//             var frame = framesReceived.Dequeue();
 
-            // The first message is the id.
-            if (id == -1)
-                id = frame.ClientId;
+//             // The first message is the id.
+//             if (id == -1)
+//                 id = frame.ClientId;
 
-            OnFrameReceived?.Invoke(frame);
+//             OnFrameReceived?.Invoke(frame);
 
-            if (callbacks.ContainsKey(frame.MessageId))
-            {
-                callbacks[frame.MessageId](frame);
-                callbacks.Remove(frame.MessageId);
-            }
-        }
-    }
+//             if (callbacks.ContainsKey(frame.MessageId))
+//             {
+//                 callbacks[frame.MessageId](frame);
+//                 callbacks.Remove(frame.MessageId);
+//             }
+//         }
+//     }
 
-    void OnClosed(WebSocket ws, UInt16 code, string message)
-    {
-        Debug.Log($"BITE WS Closed: {code} {message}");
-        webSocket = null;
-        connected = false;
-    }
+//     void OnClosed(WebSocket ws, UInt16 code, string message)
+//     {
+//         Debug.Log($"BITE WS Closed: {code} {message}");
+//         webSocket = null;
+//         connected = false;
+//     }
 
-    void OnError(WebSocket ws, string error)
-    {
-        Debug.Log($"BITE WS Error: {error}");
-        webSocket = null;
-        connected = false;
-    }
+//     void OnError(WebSocket ws, string error)
+//     {
+//         Debug.Log($"BITE WS Error: {error}");
+//         webSocket = null;
+//         connected = false;
+//     }
 
-    public void Send(string message, Action<Frame> callback = null)
-    {
-        var data = Encoding.UTF8.GetBytes(message);
-        var frame = new Frame().FeedProtocol(id, ++messageCount, data);
-        webSocket.Send(frame.Bytes);
+//     public void Send(string message, Action<Frame> callback = null)
+//     {
+//         var data = Encoding.UTF8.GetBytes(message);
+//         var frame = new Frame().FeedProtocol(id, ++messageCount, data);
+//         webSocket.Send(frame.Bytes);
 
-        if (callback != null)
-            callbacks[messageCount] = callback;
-    }
+//         if (callback != null)
+//             callbacks[messageCount] = callback;
+//     }
 
-    public void Queue(string message, Action<Frame> callback = null)
-    {
-        var data = Encoding.UTF8.GetBytes(message);
-        frameQueue.FeedProtocol(id, ++messageCount, data);
+//     public void Queue(string message, Action<Frame> callback = null)
+//     {
+//         var data = Encoding.UTF8.GetBytes(message);
+//         frameQueue.FeedProtocol(id, ++messageCount, data);
 
-        if (callback != null)
-            callbacks[messageCount] = callback;
-    }
+//         if (callback != null)
+//             callbacks[messageCount] = callback;
+//     }
 
-    public void SendQueued()
-    {
-        if (frameQueue.Bytes.Length < 1)
-            return;
+//     public void SendQueued()
+//     {
+//         if (frameQueue.Bytes.Length < 1)
+//             return;
 
-        webSocket.Send(frameQueue.Bytes);
-        frameQueue = new Frame();
-    }
+//         webSocket.Send(frameQueue.Bytes);
+//         frameQueue = new Frame();
+//     }
 
-    [ContextMenu("Close")]
-    public void Close()
-    {
-        webSocket.Close(1000, "Bye!");
-    }
+//     [ContextMenu("Close")]
+//     public void Close()
+//     {
+//         webSocket.Close(1000, "Bye!");
+//     }
 
-    [ContextMenu("Send Test Messages")]
-    public void SentTestMessages()
-    {
-        var date = DateTime.Now;
+//     [ContextMenu("Send Test Messages")]
+//     public void SentTestMessages()
+//     {
+//         var date = DateTime.Now;
 
-        for (int i = 0; i < 1000; i++)
-        {
-            Send($"#g bite.best.ws.date.{i}");
-            Send($"s bite.best.ws.date.{i} {date}");
-            Send($"g bite.best.ws.date.{i}");
-            Send($"d bite.best.ws.date.{i}");
-        }
-    }
-}
+//         for (int i = 0; i < 1000; i++)
+//         {
+//             Send($"#g bite.best.ws.date.{i}");
+//             Send($"s bite.best.ws.date.{i} {date}");
+//             Send($"g bite.best.ws.date.{i}");
+//             Send($"d bite.best.ws.date.{i}");
+//         }
+//     }
+// }
